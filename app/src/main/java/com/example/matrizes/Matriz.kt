f2f763abf941faf0391364976bf276c218c1fcae
HelvioSiqueira <helviosiqueira@outlook.com>
2022-06-13 20:11:24 -0300

    Commit comum(Operações com matrizes)

--- app/src/main/java/com/example/matrizes/Matriz.kt
+++ app/src/main/java/com/example/matrizes/Matriz.kt
@@ -1,3 +1,36 @@
 package com.example.matrizes
 
-data class Matriz (var celula: Int?)
+data class Matriz(var celula: Int?) {
+
+    private fun mostrarMatriz() {
+        fun main() {
+
+            //Cria um array de duas dimensões
+            var matriz = arrayOf<Array<Int>>()
+
+            for (x in 0..3) {
+
+                //Cria o array unidimensional de linha
+                var matriz2 = arrayOf<Int>()
+
+                for (y in 0..3) {
+
+                    //Adiciona y a matriz2
+                    matriz2 += y
+                }
+
+                //Adiciona matriz2 a matriz
+                matriz += matriz2
+            }
+
+            //Imprime a matriz
+            for (x in matriz) {
+                for (y in x) {
+                    print("$y ")
+                }
+                println()
+            }
+        }
+
+    }
+}
