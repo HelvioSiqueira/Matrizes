be88ac8b5f7e64c21b368e1f09eb6700c981fab7
HelvioSiqueira <helviosiqueira@outlook.com>
2022-06-14 18:07:02 -0300

    Commit comum(add fun de inverter matriz)

--- app/src/main/java/com/example/matrizes/Matriz.kt
+++ app/src/main/java/com/example/matrizes/Matriz.kt
@@ -24,4 +24,27 @@ data class Matriz(var celula: Int?) {
 
         return matriz
     }
+
+    fun matrizInversa(array: Array<Any>, colunas: Int): Array<Array<Any>>{
+
+        val matriz = vetorMatriz(array, colunas)
+
+
+        var matriz_I = arrayOf<Array<Any>>()
+
+        val tamanhoL = matriz.size - 1
+        val tamanhoC = matriz[0].size - 1
+
+        for(x in 0..tamanhoC){
+            var linhas = arrayOf<Any>()
+
+            for(y in 0..tamanhoL){
+                linhas += matriz[y][x]
+            }
+            matriz_I += linhas
+        }
+
+        return matriz_I
+    }
 }
+
